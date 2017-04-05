4d8ff28e3121d2035632b46a1e18b2066fc71284
Bernardo Farah <ber@bernardo.me>
2017-04-04 19:13:18 -0700

    Make typing more reliable (in general)
    Undefine IGNORE_MOD_TAP_INTERRUPT

--- keyboards/ergodox/ez/keymaps/bernardo_osx/config.h
+++ keyboards/ergodox/ez/keymaps/bernardo_osx/config.h
@@ -5,9 +5,12 @@
 #include "../../config.h"
 
 #ifdef NO_DEBUG
-#undef NO_DEBUG
+  #undef NO_DEBUG
 #endif
 
+#ifdef IGNORE_MOD_TAP_INTERRUPT
+  #undef IGNORE_MOD_TAP_INTERRUPT
+#endif
 /* #define FORCE_NKRO */
 /* #define PREVENT_STUCK_MODIFIERS */
 
